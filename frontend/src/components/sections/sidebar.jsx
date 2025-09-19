341c79705eb1b7b318d11621d3033b195d40f8fb
Israel Paul <130955174+I-paul@users.noreply.github.com>
2025-09-19 13:47:29 +0530

    text

--- frontend/src/components/sections/sidebar.jsx
+++ frontend/src/components/sections/sidebar.jsx
@@ -3,7 +3,7 @@ import Button from "../UI/button";
 
 const Sidebar = ({ setDept, dept }) => {
     const Options = ["Top News","All News", "Incentives & Regulations", "Electric Vehicles", "Connected Vehicles", "Autonomous Vehicles",
-        "Shared Mobility", "Aftermarket & Digital Retail", "Leasing & Rental", "Two Wheelers", "Trucks",
+        "Shared Mobility", "Aftermarket & Digital_Retail", "Leasing & Rental", "Two Wheelers", "Trucks",
         "Buses & Coaches", "Off-Highways"];
     return (
         <motion.section className="hidden md:flex flex-col sticky h-[90vh] md:w-[25vw] lg:w-[40vw] top-20 right-0 bg-[var(--brand-primary)] border-r-4 border-b-4 border-[var(--brand-secondary)] p-8 gap-1 rounded-br-4xl 
